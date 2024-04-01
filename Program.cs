4ce16741f22044bd3f1f8b8fa3c2bde96c25f948
SkillFactoryCSharpTest <dashulichka311214@gmail.com>
2024-04-01 22:15:59 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Исключения
+{
+    internal class Program
+    {
+        delegate void SumDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+
+            SumDelegate sumDelegate = Sum1;
+            sumDelegate += Sum2;
+            sumDelegate -= Sum2;
+            sumDelegate.Invoke(10, 5);
+            Console.WriteLine(sumDelegate);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            /*try 
+            {
+                
+                throw new RankException("Oshibka");
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+                
+            }
+            finally { Console.Read(); }*/
+
+
+        }
+        static void Sum1(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine(c);
+        }
+        static void Sum2(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+   
+}
